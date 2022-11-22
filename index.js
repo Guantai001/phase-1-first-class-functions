2a41afe4731993257f9a657bc631ce386c7688ac
guantai <jpguantai@gmail.com>
2022-11-22 19:54:54 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,12 @@
+const receivesAFunction = (callback) => {
+    callback();
+
+}
+
+const returnsANamedFunction = ()=>{
+    return function namedFunction(){}
+}
+
+const returnsAnAnonymousFunction = ()=>{
+    return function(){}
+}
